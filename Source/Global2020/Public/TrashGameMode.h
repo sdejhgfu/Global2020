c673eba8cedeb56cbf28a37dcc5728d795e4ea72
Steven McQueen <42591887+sdejhgfu@users.noreply.github.com>
2020-02-01 00:39:49 -0500

    Added GameMode
    Added our gamemode and the player bp to spawn as well

---
+++ Source/Global2020/Public/TrashGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrashGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBAL2020_API ATrashGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
